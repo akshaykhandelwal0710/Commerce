d462675fd031298c029055edf7f5ac4bd27a03b0
akshay <theakshaykhandelwal@gmail.com>
2020-11-10 17:05:09 +0530

    commit agf

--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -64,3 +64,6 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+
+def listing(request, listing):
+    pass
